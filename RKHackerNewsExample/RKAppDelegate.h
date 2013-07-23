a4b4204dae81da5769dc525a85e429c64469c71c
Samuel E. Giddins <segiddins@segiddins.me>
2013-07-23 14:37:07 -0400

    Add xcodeproj

---
+++ RKHackerNewsExample/RKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RKAppDelegate.h
+//  RKHackerNewsExample
+//
+//  Created by Samuel E. Giddins on 7/23/13.
+//  Copyright (c) 2013 RestKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RKHackerNewsExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RKHackerNewsExample
+//
+//  Created by Samuel E. Giddins on 7/23/13.
+//  Copyright (c) 2013 RestKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RKAppDelegate class]));
+    }
+}
